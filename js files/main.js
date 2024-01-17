225911dd51f9c4d0e82378a01ffc0ddc718d314b
CasperWLX <hampuskallberg@gmail.com>
2024-01-17 15:43:10 +0100

    changed to games

--- js files/main.js
+++ js files/main.js
@@ -1,4 +1,27 @@
 document.addEventListener('DOMContentLoaded',function(){
     let fadeInImage = document.getElementById('poster-image');
-    fadeInImage.style.opacity = '1';
-})
+    let backImage = document.getElementById('back-image');
+    const imageArray = [
+        '/images/frontpage images/Path-of-exile-2.jpg',
+        '/images/frontpage images/pubg.jpg',
+        '/images/frontpage images/gtav.jpg',
+        '/images/frontpage images/csgo2.jpg',
+        '/images/frontpage images/lethalcompany.jpg',
+        '/images/frontpage images/baldurs-gate-3.jpg'
+    ];
+
+    let currentImageIndex = 0;
+
+    function changeImage(){
+        backImage.src = fadeInImage.src
+        fadeInImage.style.animation = 'none';
+        fadeInImage.src = imageArray[currentImageIndex];
+        fadeInImage.offsetHeight;
+        fadeInImage.style.opacity = '1';
+        fadeInImage.style.animation = '';
+
+        currentImageIndex = (currentImageIndex + 1) % imageArray.length;
+    }
+
+    setInterval(changeImage, 5000);
+});
